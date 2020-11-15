a7bc0ae0060a3790a70d57fcf6bedf9a5ee147e3
KRTirtho <krtirtho@gmail.com>
2020-11-15 12:46:06 +0600

    initial commit after splitting up tiny-css

---
+++ tiny-css-classes.ts
@@ -0,0 +1,81 @@
+export const tinyCssClasses = {
+    /* display */
+    dyFlex: "d-flex",
+    dGrid: "d-grid",
+    dBlock: "d-block",
+    /* justify content */
+    jcCenter: "jc-center",
+    jcStart: "jc-start",
+    jcEnd: "jc-end",
+    jcAround: "jc-around",
+    jcBetween: "jc-between",
+    jcEvenly: "jc-evenly",
+    jcStretch: "jc-stretch",
+    jcBaseline: "jc-baseline",
+    /* justify self */
+    jsCenter: "js-center",
+    jsStart: "js-start",
+    jsEnd: "js-end",
+    jsStretch: "js-stretch",
+    jsBaseline: "js-baseline",
+    // align item
+    aiCenter: "ai-center",
+    aiStart: "ai-start",
+    aiEnd: "ai-end",
+    aiStretch: "ai-stretch",
+    aiBasLine: "ai-baseline",
+    // align self
+    asCenter: "as-center",
+    asStart: "as-start",
+    asEnd: "as-end",
+    asStretch: "as-stretch",
+    asBasLine: "as-baseline",
+    // align content
+    acCenter: "ac-center",
+    acStart: "ac-start",
+    acEnd: "ac-end",
+    acAround: "ac-around",
+    acBetween: "ac-between",
+    acEvenly: "ac-evenly",
+    // flex direction
+    flexColumn: "flex-column",
+    flexRow: "flex-row",
+    flexColumnReverse: "flex-column-reverse",
+    flexRowReverse: "flex-row-reverse",
+    // flex wrap, shrink, grow
+    flexWrap: "flex-wrap",
+    flexNoWrap: "flex-no-wrap",
+    flexShrink: "flex-shrink",
+    flexNoShrink: "flex-no-shrink",
+    flexGrow: "flex-grow",
+    flexNoGrow: "flex-no-grow",
+    // float
+    floatLeft: "float-left",
+    floatRight: "float-right",
+    floatNone: "float-none",
+    // position
+    pAbsolute: "p-absolute",
+    pRelative: "p-relative",
+    pFixed: "p-fixed",
+    pSticky: "p-sticky",
+    pStatic: "p-static",
+    // cursor
+    cPointer: "c-pointer",
+    cNotAllowed: "c-not-allowed",
+    cGrab: 'c-grab',
+    cProgress: 'c-progress',
+    // list style
+    listNone: "list-none",
+    listSquare: "list-square",
+    listCircle: "list-circle",
+    listDisc: "list-disc",
+    // text decoration
+    textDashed: "text-dashed",
+    textDotted: "text-dotted",
+    textDouble: "text-double",
+    textLineThrough: "text-line-through",
+    textNone: "text-none",
+    textSolid: "text-solid",
+    textUnderline: "text-underline",
+    textWavy: "text-wavy",
+}
